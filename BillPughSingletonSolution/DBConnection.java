c38a1b1b7a8c41f905a3fd0d4d5e55dec272dcc6
Himanshu <himanshusingh0608@gmail.com>
2024-06-14 22:42:28 +0530

    Bill Pugh Singleton Solution

---
+++ BillPughSingletonSolution/DBConnection.java
@@ -0,0 +1,15 @@
+package BillPughSingletonSolution;
+
+public class DBConnection {
+
+    private DBConnection(){
+
+    }
+
+    private static class DBConnectionHelper{
+        private static final DBConnection INSTANCE_OBJECT = new DBConnection();
+    }
+    public static DBConnection getInstance(){
+        return DBConnectionHelper.INSTANCE_OBJECT;
+    }
+}
